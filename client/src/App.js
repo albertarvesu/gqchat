f24feab33b3d91c482d8b7b41f2eabf713b57b51
albertarvesu <albertarvesu@gmail.com>
2018-03-14 18:21:40 +1100

    correct interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ApolloProvider, createNetworkInterface } from "react-apollo";
+import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
@@ -9,21 +9,8 @@ import { ChannelsListWithData } from "./components/ChannelsList";
 
 import "./App.css";
 
-const networkInterface = createNetworkInterface({
-  uri: "http://localhost:4000/graphql"
-});
-
-networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      setTimeout(next, 500);
-    }
-  }
-]);
-
 const client = new ApolloClient({
-  // link: new HttpLink({ uri: "http://localhost:3001/graphql" }),
-  networkInterface,
+  link: new HttpLink({ uri: "http://localhost:3001/graphql" }),
   cache: new InMemoryCache()
 });
 
