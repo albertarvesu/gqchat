4425c78cc5ddfa96dfdd2be98c591d7f1899191f
albertarvesu <albertarvesu@gmail.com>
2018-03-19 14:17:11 +1100

    added typography

---
+++ client/src/components/styled/typography.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Header = styled.h4`
+  color: #f5f5f5;
+  font-size: 1.5rem;
+`;
+
+export const MessageItem = styled.h4`
+  color: #f5f5f5;
+  font-size: 1rem;
+`;
+
+export const StyledLink = styled.h5`
+  color: #f5f5f5;
+  font-size: 1rem;
+  &:hover {
+    color: #ffcdd2;
+  }
+`;
